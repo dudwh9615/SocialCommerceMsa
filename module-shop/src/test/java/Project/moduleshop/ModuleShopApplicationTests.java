c46b237b310ea81022ebbf025c271f0c945352e6
dudwh9615 <dudwh9615@naver.com>
2024-02-19 23:32:39 +0900

    shop-service 생성 및 설정

---
+++ module-shop/src/test/java/Project/moduleshop/ModuleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.moduleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
