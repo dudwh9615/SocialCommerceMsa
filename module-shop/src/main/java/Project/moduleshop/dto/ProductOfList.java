386a946df63e4cdf91ffdee2e5f8ecf47d7edaf5
dudwh9615 <dudwh9615@naver.com>
2024-02-24 12:36:01 +0900

    상품 목록 조회 반환 DTO

---
+++ module-shop/src/main/java/Project/moduleshop/dto/ProductOfList.java
@@ -0,0 +1,11 @@
+package Project.moduleshop.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProductOfList {
+    private Long id;
+    private String name;
+}
