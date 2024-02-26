faa366748f364304cf63a6e24f01e37ae88374ae
dudwh9615 <dudwh9615@naver.com>
2024-02-26 13:03:57 +0900

    api 주소 수정 OrderService.createOrder 수정에 따른 코드 수정

--- module-shop/src/main/java/Project/moduleshop/controller/OrderController.java
+++ module-shop/src/main/java/Project/moduleshop/controller/OrderController.java
@@ -1,27 +1,29 @@
 package Project.moduleshop.controller;
 
+import Project.moduleshop.dto.OrderRequestDto;
+import Project.moduleshop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products/orders")
+@RequestMapping("/shop/orders")
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     public ResponseEntity<String> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
-        Long orderNum = orderService.createOrder(orderRequestDto).getId();
+        Long orderNum = orderService.createOrder(orderRequestDto);
         return ResponseEntity.ok(orderNum + "번 주문 생성 완료");
     }
 
-    @PostMapping("/{orderId}/payments")
-    public ResponseEntity<String> executeOrder(@PathVariable Long orderId) {
-        orderService.executeOrder(orderId);
-        return ResponseEntity.ok(orderId + "번 주문 완료");
-    }
+//    @PostMapping("/{orderId}/payments")
+//    public ResponseEntity<String> executeOrder(@PathVariable Long orderId) {
+//        orderService.executeOrder(orderId);
+//        return ResponseEntity.ok(orderId + "번 주문 완료");
+//    }
 
 //    @GetMapping
 //    public ResponseEntity<OrderResponseDto> getOrderInfo() {
