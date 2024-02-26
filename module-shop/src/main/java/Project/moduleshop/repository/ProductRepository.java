1270930d3b06fbb12ba56b060bcf252ccc8f5f20
dudwh9615 <dudwh9615@naver.com>
2024-02-26 13:07:50 +0900

    ProductRepository 생성

--- module-shop/src/main/java/Project/moduleshop/repository/ProductRepository.java
+++ module-shop/src/main/java/Project/moduleshop/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package Project.moduleshop.repository;
 
 import Project.moduleshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findById(Long id);
 }
