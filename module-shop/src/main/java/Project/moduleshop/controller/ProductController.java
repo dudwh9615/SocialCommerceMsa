a15005ba8fc37904a7fa38604e1ffd2d8fe67f4d
dudwh9615 <dudwh9615@naver.com>
2024-02-26 13:06:35 +0900

    api 주소 수정 남은 수량 api 추가 기타 url 및 리턴 타입 변경

--- module-shop/src/main/java/Project/moduleshop/controller/ProductController.java
+++ module-shop/src/main/java/Project/moduleshop/controller/ProductController.java
@@ -1,17 +1,24 @@
 package Project.moduleshop.controller;
 
+import Project.moduleshop.dto.ProductOfList;
+import Project.moduleshop.dto.ProductRequestDto;
+import Project.moduleshop.dto.ProductResponseDto;
 import Project.moduleshop.service.ProductService;
+import Project.moduleshop.service.StockService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products")
+@RequestMapping("/shop/products")
 public class ProductController {
 
     private final ProductService productService;
+    private final StockService stockService;
 
     /*
      * 상품 등록
@@ -26,23 +33,28 @@ public class ProductController {
      * 상품 목록
      */
     @GetMapping("/all")
-    public Page<ProductOfList> findAllProducts() {
+    public List<ProductOfList> findAllProducts() {
         return productService.findAllProducts();
     }
 
     /*
      * 상품 상세 페이지
      */
-    @GetMapping
-    public ResponseEntity<ProductResponseDto> findProduct(@RequestBody ProductRequestDto productRequestDto) {
-        return ResponseEntity.ok(productService.findProduct(productRequestDto));
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponseDto> findProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(productService.findProduct(productId));
+    }
+
+    @GetMapping("/{productId}/remain")
+    public ResponseEntity<Long> checkProductStock(@PathVariable Long productId) {
+        return ResponseEntity.ok(stockService.getStockInfo(productId));
     }
 
     /*
      * 남은 수량
      */
-    @GetMapping("/{productId}/stock")
-    public ResponseEntity<Long> checkProductStock(@PathVariable Long productId) {
-        return ResponseEntity.ok(productService.checkProductStock(productId));
-    }
+//    @GetMapping("/{productId}/stock")
+//    public ResponseEntity<Long> checkProductStock(@PathVariable Long productId) {
+//        return ResponseEntity.ok(productService.checkProductStock(productId));
+//    }
 }
