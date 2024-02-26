3e75e12c0c68ed9806afa4379dbb788f3043a63e
dudwh9615 <dudwh9615@naver.com>
2024-02-26 13:07:14 +0900

    order 엔티티 builder 추가

--- module-shop/src/main/java/Project/moduleshop/entity/Order.java
+++ module-shop/src/main/java/Project/moduleshop/entity/Order.java
@@ -4,14 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 @Entity(name = "Orders")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
