22f8237b624f107f1e8ada253af33588c80b09b4
dudwh9615 <dudwh9615@naver.com>
2024-02-27 15:11:44 +0900

    stock 타입 Long -> Integer로 변경

--- module-shop/src/main/java/Project/moduleshop/dto/ProductRequestDto.java
+++ module-shop/src/main/java/Project/moduleshop/dto/ProductRequestDto.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class ProductRequestDto {
     private String name;
     private Long price;
-    private Long stock;
+    private Integer stock;
     private String explain;
     private String openAt;
 }
