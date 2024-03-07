5d65272a06054c9272156c156d71cf8bb41358bb
dudwh9615 <dudwh9615@naver.com>
2024-03-08 00:00:09 +0900

    Dto 내부 getter, setter 외의 메소드 제거

--- module-activity/src/main/java/Project/SocialCommerce/dto/UserResponseDto.java
+++ module-activity/src/main/java/Project/SocialCommerce/dto/UserResponseDto.java
@@ -15,13 +15,4 @@ public class UserResponseDto {
     private String name;
     private String profile;
     private String greetings;
-
-//    public static UserResponseDto toDto(User user) {
-//        UserResponseDto dto = new UserResponseDto();
-//        dto.setName(user.getName());
-//        dto.setEmail(user.getEmail());
-//        dto.setProfile(user.getProfile());
-//        dto.setGreetings(user.getGreetings());
-//        return dto;
-//    }
 }
